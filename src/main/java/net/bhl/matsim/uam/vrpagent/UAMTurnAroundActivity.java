e05c8527301b55dd07ee4ac5104c7db8737c9f58
Raoul <RRothfeld@users.noreply.github.com>
2019-11-21 15:55:21 +0100

    Update UAMTurnAroundActivity.java

--- src/main/java/net/bhl/matsim/uam/vrpagent/UAMTurnAroundActivity.java
+++ src/main/java/net/bhl/matsim/uam/vrpagent/UAMTurnAroundActivity.java
@@ -1,7 +1,7 @@
 package net.bhl.matsim.uam.vrpagent;
 
 import net.bhl.matsim.uam.schedule.UAMTurnAroundTask;
-import org.matsim.contrib.dynagent.AbstractDynActivity;
+import org.matsim.contrib.dynagent.DynActivity;
 
 /**
  * An implementation of AbstractDynActivity for UAM DynAgents for the
@@ -9,13 +9,14 @@ import org.matsim.contrib.dynagent.AbstractDynActivity;
  *
  * @author balacmi (Milos Balac), RRothfeld (Raoul Rothfeld)
  */
-public class UAMTurnAroundActivity extends AbstractDynActivity {
+public class UAMTurnAroundActivity implements DynActivity {
 
 	final private UAMTurnAroundTask turnAroundTask;
 	private double now;
+	private String activityType;
 
 	public UAMTurnAroundActivity(UAMTurnAroundTask turnAroundTask) {
-		super(turnAroundTask.getName());
+		this.activityType = turnAroundTask.getName();
 		this.turnAroundTask = turnAroundTask;
 		this.now = turnAroundTask.getBeginTime();
 	}
@@ -23,6 +24,11 @@ public class UAMTurnAroundActivity extends AbstractDynActivity {
 	@Override
 	public void doSimStep(double now) {
 		this.now = now;
+	}
+
+	@Override
+	public String getActivityType() {
+		return activityType;
 	}
 
 	@Override
