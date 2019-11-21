01464aa2dbf880e3bb53c05984812974a1163676
Raoul <RRothfeld@users.noreply.github.com>
2019-11-21 17:31:38 +0100

    Update UAMPassengerEngine.java

--- src/main/java/net/bhl/matsim/uam/qsim/UAMPassengerEngine.java
+++ src/main/java/net/bhl/matsim/uam/qsim/UAMPassengerEngine.java
@@ -4,6 +4,7 @@ import org.matsim.api.core.v01.network.Network;
 import org.matsim.contrib.dvrp.optimizer.VrpOptimizer;
 import org.matsim.contrib.dvrp.passenger.PassengerEngine;
 import org.matsim.contrib.dvrp.passenger.PassengerRequestCreator;
+import org.matsim.contrib.dvrp.passenger.PassengerRequestValidator;
 import org.matsim.core.api.experimental.events.EventsManager;
 
 /**
@@ -14,8 +15,8 @@ import org.matsim.core.api.experimental.events.EventsManager;
 public class UAMPassengerEngine extends PassengerEngine {
 
 	public UAMPassengerEngine(String mode, EventsManager eventsManager, PassengerRequestCreator requestCreator,
-							  VrpOptimizer optimizer, Network network) {
-		super(mode, eventsManager, requestCreator, optimizer, network);
+							  VrpOptimizer optimizer, Network network, PassengerRequestValidator requestValidator) {
+		super(mode, eventsManager, requestCreator, optimizer, network, requestValidator);
 	}
 
 }
