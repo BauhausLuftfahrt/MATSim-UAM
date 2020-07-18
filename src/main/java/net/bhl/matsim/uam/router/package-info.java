5f90529d25a47ea37c334a7508a597e95097167a
RRothfeld <raoul.rothfeld+github@gmail.com>
2020-07-18 13:13:01 +0200

    Update package-info.java

--- src/main/java/net/bhl/matsim/uam/router/package-info.java
+++ src/main/java/net/bhl/matsim/uam/router/package-info.java
@@ -1,8 +1,4 @@
 /**
  * @author balacmi (Milos Balac), RRothfeld (Raoul Rothfeld)
  */
-/**
- * @author balacmi (Milos Balac), RRothfeld (Raoul Rothfeld)
- *
- */
 package net.bhl.matsim.uam.router;
