5144a80c4e5e793d7270ca110ecb61922d384aba
balacmi <milos.balac@ivt.baug.ethz.ch>
2021-03-09 22:24:29 +0100

    fix integration test
    removed all routes as they were not compatible with regular matsim
    
    added uam to subtour mode choice

--- src/test/java/net/bhl/matsim/uam/RunCorsicaIT.java
+++ src/test/java/net/bhl/matsim/uam/RunCorsicaIT.java
@@ -3,6 +3,7 @@ package net.bhl.matsim.uam;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 import org.matsim.api.core.v01.Id;
@@ -10,6 +11,10 @@ import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.network.NetworkFactory;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
 import org.matsim.contrib.dvrp.run.DvrpModule;
 import org.matsim.core.config.Config;
@@ -18,7 +23,10 @@ import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ActivityParams;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ModeParams;
 import org.matsim.core.config.groups.QSimConfigGroup.StarttimeInterpretation;
 import org.matsim.core.config.groups.StrategyConfigGroup.StrategySettings;
+import org.matsim.core.config.groups.SubtourModeChoiceConfigGroup;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.router.TripStructureUtils;
+import org.matsim.core.router.TripStructureUtils.Trip;
 import org.matsim.core.scenario.ScenarioUtils;
 
 import com.google.common.collect.ImmutableSet;
@@ -50,7 +58,8 @@ public class RunCorsicaIT {
 					strategy.setStrategyName("ChangeExpBeta");
 				}
 			}
-
+			String[] modes = { "car", "pt", "walk", "uam", "bike" };
+			((SubtourModeChoiceConfigGroup) config.getModules().get("subtourModeChoice")).setModes(modes);
 			config.strategy().setPlanSelectorForRemoval("WorstPlanSelector");
 			config.transit().setUseTransit(true);
 			config.transit().setUsingTransitInMobsim(true);
@@ -117,6 +126,20 @@ public class RunCorsicaIT {
 				link.setFreespeed(100.0);
 				link.setCapacity(1000000.0);
 				network.addLink(link);
+			}
+		}
+
+		for (Person person : scenario.getPopulation().getPersons().values()) {
+			for (Plan plan : person.getPlans()) {
+				for (Trip trip : TripStructureUtils.getTrips(plan)) {
+
+					for (PlanElement pe : trip.getTripElements()) {
+
+						if (pe instanceof Leg) {
+							((Leg) pe).setRoute(null);
+						}
+					}
+				}
 			}
 		}
 
